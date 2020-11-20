130ffff18ec15940935d324f21e1b5bbd4ba16cd
Naoki Kinoshita <uttnaoki@gmail.com>
2020-11-20 23:32:47 +0900

    mod GenLectData

--- GenLectData.py
+++ GenLectData.py
@@ -1,4 +1,5 @@
 import json
+import itertools
 
 def get_corner(coors):
     x_min = y_min = 400
@@ -15,23 +16,33 @@ def get_corner(coors):
     return (x_min, x_max, y_min, y_max)
 
 def conv_lect_feature(feature):
+    result = feature
     this_polygon = feature["geometry"]["coordinates"]
+
+    # 配列の深さが位置段階深い場合があるので修正
+    if len(this_polygon) > 1:
+        this_polygon = list(itertools.chain.from_iterable(this_polygon))
+
+
     corners = get_corner(this_polygon[0])
-    lect_polygon = this_polygon
-    lect_polygon[0] = [
+    result["geometry"]["coordinates"][0] = [
         [corners[0], corners[3]],
         [corners[1], corners[3]],
         [corners[1], corners[2]],
         [corners[0], corners[2]]
     ]
-    return lect_polygon
+    return result
 
 if __name__ == "__main__":
-    with open("source/fukuoka_park.geojson") as f:
+    with open("source/20200401_fukuoka_park.geojson") as f:
+    # with open("source/fukuoka_park.geojson") as f:
         geojson = json.load(f)
     
-    lect_features = [conv_lect_feature(f) for f in geojson["features"]]
+    lect_features = [
+        conv_lect_feature(f) for f in geojson["features"]
+        if len(f["geometry"]["coordinates"]) > 0 ]
     geojson["features"] = lect_features
 
-    with open("output/fukuoka_park_lect.geojson", "w") as f:
-        json.dump(geojson, f)
+    with open("output/fukuoka_park_lect.geojson", "w", encoding='utf8') as f:
+        json.dump(geojson, f, ensure_ascii=False)
+
